21ec8c25d9750165763fdb839031d30d46e6cef7
Karna Sjoberg <kesjoberg@gmail.com>
2021-09-29 18:00:30 -0500

    Added all the rowns and the code to save and retrieve items from local storage.

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+//Selectors
+var currentTime = moment().format('HH'); 
+var allTheNotes = JSON.parse(localStorage.getItem("allTheNotes")) || [];
+
+//Functions
+
+setColors();
+enterEvent();
+setDate();
+fillEvents();
+
+//Setting the colors
+function setColors() {
+  $('input').each(function () {
+    var dataTime = $(this).data("time");
+    if (dataTime < currentTime) {
+      $(this).addClass('past');
+    } else
+    if (dataTime == currentTime){
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  })
+}
+function setDate(){
+  var date = moment().format('dddd, MMMM Do, YYYY');
+  $("#currentDay").append(date);
+}
+
+function enterEvent() {
+  $('.saveBtn').on('click',function(){
+    allTheNotes = [];   
+    for( var i = 0; i<9; i++){
+      var textInput = $($("input")[i]).val();
+      var notes= textInput;
+      console.log('click');
+      allTheNotes.push(notes);
+    }
+    localStorage.setItem('allTheNotes', JSON.stringify(allTheNotes));
+  }); 
+  fillEvents()
+}
+
+function fillEvents() {
+  var currentIndex = 0
+  $('input').each( function () {
+    $(this).val(allTheNotes[currentIndex]);
+    currentIndex++;
+  })
+};
+
